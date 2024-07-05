3b0c143401c8c1a85834d83296c1162d5e12176b
bayronsp <bayron.sp5@gmail.com>
2024-07-05 02:51:17 -0400

    crear json para hf

--- Dataset/crear_dataset.py
+++ Dataset/crear_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # Cargar el archivo de Excel
-file_path = 'preguntas_y_respuestas.xlsx'  # Reemplaza con la ruta de tu archivo Excel
+file_path = 'reglamento.xlsx' 
 df = pd.read_excel(file_path)
 
 # Crear el diccionario con las preguntas y respuestas
@@ -15,7 +15,7 @@ data = {
 qa_list = [{"text": f'<s>[INST] {pregunta.strip()} [/INST] {respuesta.strip()} </s>'} for pregunta, respuesta in zip(data["Pregunta"], data["Respuesta"])]
 
 # Escribir la lista en un archivo JSONL
-with open('preguntas_respuestas_transformadas.jsonl', 'w', encoding='utf-8') as f:
+with open('train.jsonl', 'w', encoding='utf-8') as f:
     for qa in qa_list:
         json.dump(qa, f, ensure_ascii=False)
         f.write('\n')
